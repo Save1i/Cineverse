eb222059ecfd10b5a0729f53ce931ba5c4d34de4
Savelii Kosevich <savva0512@gmail.com>
2024-01-09 21:51:16 +0300

    Many things

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function animateButton() {
-  var button = document.querySelector(".promo__btn");
+  const button = document.querySelector(".promo__btn");
   button.classList.add("clicked"); // добавляем класс для анимации
 
   // Ждем некоторое время и удаляем класс
@@ -9,6 +9,20 @@ function animateButton() {
 
   setTimeout(function () {
     window.location.href = "subscription.html"; // переход на страницу subscription.html
+  }, 300);
+}
+
+function animateButtonSub() {
+  const button = document.querySelector(".promo__btn");
+  button.classList.add("clicked"); // добавляем класс для анимации
+
+  // Ждем некоторое время и удаляем класс
+  setTimeout(function () {
+    button.classList.remove("clicked");
+  }, 200);
+
+  setTimeout(function () {
+    window.location.href = "login.html"; // переход на страницу subscription.html
   }, 300);
 }
 
@@ -47,34 +61,6 @@ function toggleActive(addClass, removeClass) {
   }
 }
 
-// function animateButton__login() {
-//   const button = document.querySelector(".register__btn");
-//   button.classList.add("clicked"); // добавляем класс для анимации
-
-//   // Ждем некоторое время и удаляем класс
-//   setTimeout(function () {
-//     button.classList.remove("clicked");
-//   }, 200);
-
-//   setTimeout(function () {
-//     window.location.href = "login__input-number.html"; // переход на страницу subscription.html
-//   }, 300);
-// }
-
-// function animateButton__login_inputnumber() {
-//   const button = document.querySelector(".login__btn");
-//   button.classList.add("clicked"); // добавляем класс для анимации
-
-//   // Ждем некоторое время и удаляем класс
-//   setTimeout(function () {
-//     button.classList.remove("clicked");
-//   }, 200);
-
-//   setTimeout(function () {
-//     window.location.href = "login__general-information.html"; // переход на страницу subscription.html
-//   }, 300);
-// }
-
 function transitionButton(addTransition) {
   const button = document.getElementById(`${addTransition}`);
   button.classList.add("clicked"); // добавляем класс для анимации
@@ -99,4 +85,62 @@ function transitionButton(addTransition) {
 
     // переход на страницу subscription.html
   }, 150);
+}
+
+// Переключение видимости выпадающего списка
+function toggleDropdown() {
+  const dropdownContent = document.getElementById("myDropdown");
+  dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
+}
+
+function changeGender(selectedGender) {
+  const genderText = document.getElementById("gender");
+  const genderW = document.getElementById("woman");
+  const dropdownContent = document.getElementById("myDropdown");
+
+  if (dropdownContent.style.display === "block" || selectedGender === "woman") {
+    switch (genderText.textContent) {
+      case "Мужской":
+        genderText.innerHTML = "Женский";
+        genderW.innerHTML = "Мужской";
+        break;
+      case "Женский":
+        genderText.innerHTML = "Мужской";
+        genderW.innerHTML = "Женский";
+        break;
+    }
+  }
+  toggleDropdown(); // Закрываем выпадающий список после выбора
+}
+
+// Закрываем выпадающий список при клике вне его области
+window.onclick = function (event) {
+  const dropdownInput = document.getElementById("dropdownInput");
+  if (event.target !== dropdownInput && !dropdownInput.contains(event.target)) {
+    const dropdownContent = document.getElementById("myDropdown");
+    dropdownContent.style.display = "none";
+  }
+};
+
+const headerBtn = document.querySelector(".header__btn");
+const headerMenu = document.querySelector(".header__menu-container");
+
+headerBtn.addEventListener("click", () => {
+  headerMenu.classList.toggle("menu-open");
+});
+
+function transitionPoster(addTransition) {
+  const imgBtn = document.getElementById(`${addTransition}`);
+
+  switch (addTransition) {
+    case "oppenheimer-vert":
+      window.location.href = "oppenheimer-page.html";
+      break;
+    case "login__btn-input-number":
+      window.location.href = "login__general-information.html";
+      break;
+    case "login__btn-general-info":
+      window.location.href = "personal-area.html";
+      break;
+  }
 }
